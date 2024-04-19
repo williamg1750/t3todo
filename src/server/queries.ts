4ada506536f5ca6b15ac553060bcb32ac2f1c213
William <74834468+williamg1750@users.noreply.github.com>
2024-04-19 13:30:16 -0400

    fix build error

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -10,6 +10,7 @@ import { auth } from "@clerk/nextjs/server";
 export async function getTodos() {
   const user = auth();
   console.log("user", user);
+  if (!user.userId) throw new Error("Unauthorized");
   const todos = await db.query.todo.findMany({
     where: (model, { eq }) => eq(model.userId, user.userId),
     orderBy: (model, { desc }) => desc(model.createdAt),
