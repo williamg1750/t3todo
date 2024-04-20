3ef47735de48bdd029a89c99d9d1036d4e6f8aed
William <74834468+williamg1750@users.noreply.github.com>
2024-04-19 21:02:42 -0400

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,20 +8,15 @@ export default function HomePage() {
           T3<span className="text-[hsl(280,100%,70%)]">Todo</span>
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Simple Todo app →</h3>
+          <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
+            <h3 className="text-2xl font-bold">Simple Todo app</h3>
             <div className="text-lg">Just a todo app with a twist</div>
-          </Link>
+          </div>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
+            href="https://t3todo-l6gzn41o3-williams-projects-f986bfaa.vercel.app/todos"
           >
-            <h3 className="text-2xl font-bold">Get Started</h3>
+            <h3 className="text-2xl font-bold">Get Started →</h3>
             <div className="text-lg">
               Login via Gmail or Discord to get started
             </div>
